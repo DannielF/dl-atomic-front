ca4c99152612bdb2a6fbba1c420fd8013029c977
daniel.granados <daniel.granados@sofka.com.co>
2023-06-14 11:55:54 -0500

    Add: login-button css

--- src/components/atoms/LoginButton.tsx
+++ src/components/atoms/LoginButton.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import styles from './LoginButton.module.css';
 import { useAuth0 } from '@auth0/auth0-react';
+
+const classCss = `btn btn-primary ${styles.login_button}`;
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return (
+    <button
+      type="button"
+      className={classCss}
+      onClick={() => loginWithRedirect()}
+    >
+      Log In
+    </button>
+  );
 };
 
 export default LoginButton;
