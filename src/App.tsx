d4e25d8e3170add20fdc372fca3f550b609a1b7e
daniel.granados <daniel.granados@sofka.com.co>
2023-06-14 11:48:50 -0500

    Add: login-logout button, router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import styles from './App.module.css';
+import LoginButton from './components/atoms/LoginButton';
+
+const classCss = `${styles.homeLogin}`;
 
 function App() {
-  return <></>;
+  return (
+    <main className={classCss}>
+      <h2>WalletApp</h2>
+      <LoginButton />
+    </main>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 const authConfig = {
@@ -14,8 +15,10 @@ const authConfig = {
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Auth0Provider {...authConfig}>
-      <App />
-    </Auth0Provider>
+    <BrowserRouter>
+      <Auth0Provider {...authConfig}>
+        <App />
+      </Auth0Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
