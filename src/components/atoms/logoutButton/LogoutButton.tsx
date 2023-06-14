5dd77c6765c69f5094d211a42b70a409bfd1c7b9
daniel.granados <daniel.granados@sofka.com.co>
2023-06-14 11:59:49 -0500

    Add: logout-button css

--- src/components/atoms/logoutButton/LogoutButton.tsx
+++ src/components/atoms/logoutButton/LogoutButton.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import styles from './LogoutButton.module.scss';
 import { useAuth0 } from '@auth0/auth0-react';
+
+const classCss = `btn btn-warning ${styles.logout_button}`;
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
     <button
+      type="button"
+      className={classCss}
       onClick={() =>
         logout({ logoutParams: { returnTo: window.location.origin } })
       }
