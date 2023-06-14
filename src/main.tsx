f6b4862edd73a9ddc93a6f8a15e9fae5aea6fd67
daniel.granados <daniel.granados@sofka.com.co>
2023-06-14 10:48:58 -0500

    Add: auth0 provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { Auth0Provider } from '@auth0/auth0-react';
+import './index.css';
+
+const authConfig = {
+  domain: import.meta.env.AUTH0_DOMAIN,
+  clientId: import.meta.env.AUTH0_CLIENT_ID,
+  authorizationParams: {
+    redirect_uri: window.location.origin
+  }
+};
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider {...authConfig}>
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
