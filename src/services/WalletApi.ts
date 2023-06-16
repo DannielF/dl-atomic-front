ca7fa14c6f6cd254736d72cf3db99859a0487bd6
daniel.granados <daniel.granados@sofka.com.co>
2023-06-16 09:00:50 -0500

    Add: getAccessToken hook

--- src/services/WalletApi.ts
+++ src/services/WalletApi.ts
@@ -1,23 +1,8 @@
-import { useAuth0 } from '@auth0/auth0-react';
 import { Client } from '../domain/entities/Client';
 import { Transaction } from '../domain/entities/Transaction';
+import { GetAccessToken } from './GetAccessTokenHook';
 
 const API_URL: string = import.meta.env.VITE_API_URL;
-
-const GetAccessToken = async () => {
-  const { getAccessTokenSilently } = useAuth0();
-
-  try {
-    return await getAccessTokenSilently({
-      authorizationParams: {
-        audience: import.meta.env.VITE_API_AUDIENCE,
-        redirect_uri: window.location.origin
-      }
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
 
 const getHeaders = async () => {
   const token = await GetAccessToken();
