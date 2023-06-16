aec29bd65f87a1d39adeece6c27fcccf1c9754b4
daniel.granados <daniel.granados@sofka.com.co>
2023-06-16 11:44:30 -0500

    upd: login, signup store client

--- src/components/atoms/loginButton/LoginButton.tsx
+++ src/components/atoms/loginButton/LoginButton.tsx
@@ -1,23 +1,28 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import styles from './LoginButton.module.css';
+import { useDispatch } from 'react-redux';
+import { clientWallet } from '../../../shared/asyncThunks/AsyncThunks';
 
 const classCss = `btn btn-primary ${styles.button__login}`;
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, user, isAuthenticated } = useAuth0();
+  const dispatch = useDispatch();
+
+  const handleLogin = async () => {
+    await loginWithRedirect({
+      appState: {
+        target: '/dashboard'
+      }
+    });
+    if (isAuthenticated) {
+      const userEmail = user?.email ?? '';
+      dispatch(clientWallet(userEmail) as any);
+    }
+  };
 
   return (
-    <button
-      type="button"
-      className={classCss}
-      onClick={() =>
-        loginWithRedirect({
-          appState: {
-            target: '/dashboard'
-          }
-        })
-      }
-    >
+    <button type="button" className={classCss} onClick={handleLogin}>
       Log In
     </button>
   );
--- src/components/atoms/signupButton/SignupButton.tsx
+++ src/components/atoms/signupButton/SignupButton.tsx
@@ -1,10 +1,13 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import styles from './SignupButton.module.css';
+import { useDispatch } from 'react-redux';
+import { createClientWallet } from '../../../shared/asyncThunks/AsyncThunks';
 
 const classCss = `btn btn-secondary ${styles.button__sign_up}`;
 
 export const SignupButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, user, isAuthenticated } = useAuth0();
+  const dispatch = useDispatch();
 
   const handleSignUp = async () => {
     await loginWithRedirect({
@@ -16,6 +19,10 @@ export const SignupButton = () => {
         screen_hint: 'signup'
       }
     });
+    if (isAuthenticated) {
+      const userEmail = user?.email ?? '';
+      dispatch(createClientWallet(userEmail) as any);
+    }
   };
 
   return (
