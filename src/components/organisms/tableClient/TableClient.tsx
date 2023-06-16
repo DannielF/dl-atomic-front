2bb0b376c210f85521b14559f6d39179f3ead43b
daniel.granados <daniel.granados@sofka.com.co>
2023-06-16 13:12:08 -0500

    Add: tables client, transactions

---
+++ src/components/organisms/tableClient/TableClient.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const TableClient = () => {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody className="table-group-divider"></tbody>
+    </table>
+  );
+};
