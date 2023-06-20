f5a793f02f54d40f5e16a67608d626032bf23607
daniel.granados <daniel.granados@sofka.com.co>
2023-06-20 16:07:59 -0500

    Add: createWallet page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Route, Routes } from 'react-router-dom';
 import { Error } from './components/molecules/errorAuth/ErrorAuth';
 import { Loading } from './components/molecules/loading/Loading';
+import { TableClients } from './components/organisms/tableClients/TableClients';
+import { TableTransactions } from './components/organisms/tableTransactions/TableTransactions';
+import { CreateWallet } from './components/pages/createWallet/CreateWallet';
 import { Dashboard } from './components/pages/dashboard/Dashboard';
 import { Home } from './components/pages/home/Home';
 import { AuthenticationGuard } from './guards/AuthenticationGuard';
-import { TableClients } from './components/organisms/tableClients/TableClients';
-import { TableTransactions } from './components/organisms/tableTransactions/TableTransactions';
 
 function App() {
   const { isLoading, error } = useAuth0();
@@ -15,14 +17,21 @@ function App() {
     return <Loading />;
   }
 
+  const MemoizedCreateWallet = React.memo(CreateWallet);
+  const MemoizedDashboard = React.memo(Dashboard);
+
   return (
     <>
       {error && <Error message={error.message} />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route
+          path="createWallet"
+          element={<AuthenticationGuard component={MemoizedCreateWallet} />}
+        />
+        <Route
           path="dashboard"
-          element={<AuthenticationGuard component={() => <Dashboard />} />}
+          element={<AuthenticationGuard component={MemoizedDashboard} />}
         >
           <Route path="clients" element={<TableClients />} />
           <Route path="transactions" element={<TableTransactions />} />
---
+++ src/components/pages/createWallet/CreateWallet.tsx
@@ -0,0 +1,30 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { createClientWallet } from '../../../shared/asyncThunks/AsyncThunks';
+import { AppDispatch } from '../../../shared/store/Store';
+import styles from './createWallet.module.css';
+
+const classCss = `${styles.createWallet__page}`;
+
+export const CreateWallet = () => {
+  const { user } = useAuth0();
+  const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleCreateWallet = async () => {
+    const userAuthEmail = user?.email;
+    console.log('handleCreateW', userAuthEmail);
+    dispatch(createClientWallet(userAuthEmail ?? ''));
+    navigate('/dashboard');
+  };
+
+  return (
+    <main className={classCss}>
+      <h2>Create Wallet</h2>
+      <button className="btn btn-primary" onClick={handleCreateWallet}>
+        New Wallet !!!
+      </button>
+    </main>
+  );
+};
