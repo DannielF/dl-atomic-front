94e5fe8e7640ffaa6189817f26a23efff9bdfe1e
daniel.granados <daniel.granados@sofka.com.co>
2023-06-22 09:30:07 -0500

    Upd: env SPA

--- src/services/WalletApi.ts
+++ src/services/WalletApi.ts
@@ -6,7 +6,7 @@ import { GetAccessToken } from './GetAccessTokenHook';
 const API_URL: string = import.meta.env.VITE_API_URL;
 
 const getHeaders = async () => {
-  //const token = await GetAccessToken();
+  const token = await GetAccessToken();
   return {
     Authorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9CdUc3SXFVd3FpYjJPbUtxY1UycyJ9.eyJpc3MiOiJodHRwczovL2Rldi14Z2p3MWZqcTMxN2hucW5uLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiI4TkxiMUF5Tk55V2Y5V3dicFNEYXNibnN0ZTRncVdwc0BjbGllbnRzIiwiYXVkIjoiaHR0cDovL2JhY2stZGwuY29tIiwiaWF0IjoxNjg3MzgzMzIzLCJleHAiOjE2ODc0Njk3MjMsImF6cCI6IjhOTGIxQXlOTnlXZjlXd2JwU0Rhc2Juc3RlNGdxV3BzIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.KO6Zj3dj2yHBS92zErEDoME2n-zE1WNgFjjs4-MAqZRVPhqI_eqc8RARpQL65d4-XMigyQD-OTtSCbrhgUhuiiF2ZFP1hnbZSr4bAMp5kQXPnKg4p-H0Wdn_0pE5loWHK1zXC5udNkn7ru-HeePwZ1VckWdfVD9WRGzCYbZKIebmTZxbYM04ahA62QN-dolgxCjjwIIL2BEk9_1vIVURPo94Qx-poRMGwLMZVwaujDvK-MtgO99GjFQxKVIxAh6VYZe3PgsVyrDYGhoC0FOUvVXsmL2ZdqhcW7pxTL3sd3o9Ry0koCsTabhbJ1uFxPPG4DFB_pmh6TISC5vJ3gF7cA`,
     'Content-Type': 'application/json'
