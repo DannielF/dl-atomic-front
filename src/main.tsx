77ca2446d1f066f75a7eac9b2e79cc6574bfb0b7
daniel.granados <daniel.granados@sofka.com.co>
2023-06-29 12:02:12 -0500

    Add: useToken

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
-import { Auth0ProviderWithRedirectCallback } from './config/AuthProviderWithNavigate.tsx';
+import { Auth0ProviderWithRedirectCallback } from './config/security/AuthProviderWithNavigate.tsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './shared/store/Store.ts';
